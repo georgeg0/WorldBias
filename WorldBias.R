52b3141cdd8aa4f72f5e133f3acd0988a638d84a
georgeg0 <ggittu@us-m072362.biogenidec.com>
2016-12-17 21:27:48 -0500

    World Implicit Bias V1
    - Adding the consolidated CSV file
    - Changing read from CSV file
    - Identifying the country codes that are not available and reevaluating
    fields
    - Changing the directory structure

--- WorldBias.R
+++ WorldBias.R
@@ -9,23 +9,25 @@ library(foreign)
 library(plyr)
 library(raster)
 
+# ---------- Setting Path
+setwd("/Documents/WorldBias")
 
 # ---------- get data
-#You will need to download the RDS file from Git LFS directly
-#gtd <- read.spss("/Users/ggittu/Desktop/PjtImplicit/Race IAT.public.2002-2015.sav", to.data.frame=TRUE)
-gtd <- readRDS("RaceIAT.public.2002-2015.rds")
+#Reading the consolidated CSV file
+gtd <- read.csv("Data/Race.IAT.2004-2015.cleansed.csv")
 
 # ---------- data munging
-gtd.recent <- aggregate(D_biep.White_Good_all~country,gtd,mean)
-
+gtd.recent <- aggregate(D_biep.White_Good_all~countrycit,gtd,mean)
 
 # --------- draw map
-world <- readOGR(dsn=path.expand("/Users/ggittu/Desktop/WorldBiasProject"),layer="world_country_admin_boundary_shapefile_with_fips_codes")
+world <- readOGR(dsn=path.expand("WorldMapShapes"),layer="world_country_admin_boundary_shapefile_with_fips_codes")
 ggplot() +  geom_polygon(data=world, aes(x=long, y=lat, group=group))
+# Reevaluating country codes to match the country code in IAT file
+world@data$FIPS_CNTRY <- as.character (revalue(world@data$FIPS_CNTRY, replace=c(AA="AW",AC="AG",AG="DZ",AJ="AZ",AN="AD",AQ="AS",AS="AU",AU="AT",AV="AI",AY="AQ",BA="BH",BC="BW",BD="BM",BF="BS",BG="BD",BH="BZ",BK="BA",BL="BO",BN="BJ",BO="BY",BP="SB",BU="BG",BX="BN",BY="BI",CB="KH",CD="TD",CE="LK",CF="CG",CG="CD",CH="CN",CI="CL",CJ="KY",CK="CC",CN="KM",CQ="MP",CS="CR",CT="CF",CW="CK",DO="DM",DR="DO",DA="DK",EI="IE",EK="GQ",EN="EE",ES="SV",EZ="CZ",FG="GF",FP="PF",FQ="XXX",FS="TF",GA="GM",GB="GA",GG="GE",GJ="GD",GK="XX",GM="DE",GO="XX",GQ="GU",GV="GN",GZ="XX",HA="HT",HO="HN",HQ="XX",IC="IS",IM="XX",IS="XX",IV="CI",IZ="IQ",JA="JP",JE="XX",JN="XX",JQ="XX",JU="XX",KN="KP",KR="KI",KS="KR",KT="CX",KU="KW",LE="LB",LG="LV",LH="LT",LI="LR",LO="SK",LS="LI",LT="LS",MA="MG",MB="MQ",MF="YT",MG="MN",MH="MS",MI="MW",MN="MC",MO="MA",MP="MU",MQ="XX",MU="OM",MW="XXX",NE="NU",NG="NE",NH="VU",NI="NG",NL="NL",NS="SR",NT="XXX",NU="NI",PA="PY",PC="PN",PF="XX",PG="XX",PM="PA",PO="PT",PP="PG",PS="PW",PU="GW",RM="MH",RP="PH",RQ="PR",RS="RU",SB="PM",SC="KN",SE="SC",SF="ZA",SG="SN",SN="SG",SP="ES",SR="XXX",ST="LC",SU="SD",SV="SJ",SW="SE",SX="GS",SZ="CH",TD="TT",TI="TJ",TK="TC",TL="TK",TM="XXX",TN="TO",TO="TG",TP="ST",TS="TN",TU="TR",TX="TM",UP="XXX",UV="BF",VI="VG",VM="VN",VQ="VI",VT="VA",WA="NA",WE="XX",WI="XX",WQ="XX",WZ="SZ",YM="YE",ZA="ZM",ZI="ZW",UP="UA")))
 countries <- world@data
 countries <- cbind(id=rownames(countries),countries)
 countries <- merge(countries,gtd.recent, 
-                   by.x="FIPS_CNTRY", by.y="country", all.x=T)
+                   by.x="FIPS_CNTRY", by.y="countrycit", all.x=T)
 map.df <- fortify(world)
 map.df <- merge(map.df,countries, by="id")
 
