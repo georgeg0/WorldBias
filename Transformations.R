119e570b8ecb5f21392f3adef53c56fdb7815934
Tom Stafford <tom@idiolect.org.uk>
2017-01-08 16:34:54 +0000

    moving towards x-platform

--- Transformations.R
+++ Transformations.R
@@ -1,29 +1,79 @@
-###### Reading statistical file to convert it to csv ######
+###### Reading raw data files (spss) to convert it to csv ######
+
+# Citation for data
+# Xu, K., Nosek, B., & Greenwald, A. (2014). Psychology data from the race implicit association test on the project implicit demo website. Journal of Open Psychology Data, 2(1).
+# openpsychologydata.metajnl.com/articles/10.5334/jopd.ac/ 
+
+# visit https://osf.io/52qxl/ to acquire the following data files
+# 'Race IAT.public.2004.sav',
+# 'Race IAT.public.2009.sav',
+# 'Race IAT.public.2014.sav',
+# 'Race IAT.public.2011.sav',
+# 'Race IAT.public.2013.sav',
+# 'Race IAT.public.2008.sav',
+# 'Race IAT.public.2012.sav',
+# 'Race IAT.public.2010.sav',
+# 'Race IAT.public.2005.sav',
+# 'Race IAT.public.2006.sav',
+# 'Race IAT.public.2015.sav',
+# 'Race IAT.public.2007.sav'
+
+# Specify location of these files below, by setting dataloc variable
+
 
 library(foreign)
-dataset1 = read.spss('/george/Desktop/RACIAL-IAT/Race IAT.public.2015.sav', to.data.frame=TRUE)
-#Get the fields that youa re interested in
-dd <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres")]
-write.csv(dd, file='/george/Desktop/RACIAL-IAT/Race.IAT.public.2015.csv')
+
+# ---------- Setting Path
+if(TRUE) {
+  setwd("/home/tom/Dropbox/WorldBias") #tom's laptop
+  dataloc='/home/tom/Dropbox/university/expts/WorldBias/data/raw/'
+} else { 
+  setwd("/george/Desktop/RACIAL-IAT/")
+  dataloc='/george/Desktop/RACIAL-IAT/'
+}
+
+# ---------- load raw data
+#dataset1 = read.spss(file.path(dataloc,'Race IAT.public.2015.sav'), to.data.frame=TRUE)
+
+if(!require(memisc)){install.packages("memisc", repos="http://R-Forge.R-project.org")} 
+library("memisc")
+dataset1 <- data.frame(as.data.set(spss.system.file(file.path(dataloc,'Race IAT.public.2015.sav'))))
+
+#Get the fields that you are interested in
+#For some reason this first column was not capitalised in my data
+datafile2015="Race.IAT.public.2015.csv"
+dd <- dataset1[,c("d_biep.white_good_all","countrycit","countryres","raceomb","raceombmulti","ethnicityomb")]
+write.csv(dd, file=file.path('data',datafile2015))
+
+#race = White:6, ethnicity = White:5
 
 ###### cleaning the file for unwanted entries in countrycit ######
 
+if(!require(plyr)){install.packages("plyr")} 
 library(plyr)
-setwd("/Users/george/Desktop/pjt/RACIAL-IAT/CSV/2014-2015/")
-df <- read.csv("Race.IAT.public.2015.csv", header=TRUE)
+
+#setwd("/Users/george/Desktop/pjt/RACIAL-IAT/CSV/2014-2015/")
+#because we are loading a file created by this script, I haven't respected the old data organisation (i.e. I changed the folder names)
+
+df <- read.csv(file.path('data',datafile2015), header=TRUE)
 df2 <- df[!df$countrycit %in% c(". ","  ",".",".   ","    "),]
 ## removing an unwanted column carried over
 cols.dont.want <- "X"
 df3 <- df2[, ! names(df2) %in% cols.dont.want, drop = F]
-write.csv(df3, "cleansed/Race.IAT.public.cleansed.2015.csv", row.names=FALSE, quote=FALSE)
+write.csv(df3, "data/Race.IAT.public.cleansed.2015.csv", row.names=FALSE, quote=FALSE)
 count(df2, 'countrycit')
 
 ###### Combining ######
 
 # This code is for merging the csv file
-# Make sure to put all the files in a folder
+# Make sure to put all the files in a folder specified in variable dataloc
+#
+# - assumes files are CSV, but they are in .SAV from the OSF page
+#
+#
+
 setwd("/george/Desktop/pjt/RACIAL-IAT/CSV/cleansed2004-2015")
-files <- list.files(pattern = "\\.csv$")
+files <- list.files(path=dataloc, pattern = "\\.csv$")
 DF <-  read.csv(files[1])
 for (f in files[-1]) DF <- rbind(DF, read.csv(f))   
 write.csv(DF, "Race.IAT.2004-2015.csv", row.names=FALSE, quote=FALSE)
