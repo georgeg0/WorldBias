36d8f2c6012ade4445a020da60836beedc49f980
Gittu George <ggeorge@us-m072362.biogenidec.com>
2017-03-05 18:53:55 -0500

    Not considering n<100 when plotting

--- Statmake.R
+++ Statmake.R
@@ -1,10 +1,12 @@
 ###### 
+#
 #This piece of code creates scatter plot for Avg IAT vs Sample size, Helps to better understand the data
+#
 ######
 library(plyr)
 
 # Read the stat file of european countries  (sample size)
-df2 <- read.csv("/Users/ggeorge/Downloads/stats/Race.IAT.2004-2015-white-europe-stats.csv")
+df2 <- read.csv("/george/Desktop/RACIAL-IAT/data/cleansedtest/Race.IAT.2004-2015-white-europe-stats.csv")
 setwd("/george/Desktop/RACIAL-IAT/data/cleansedtest")
 
 #Reading the consolidated CSV file for Europe
@@ -15,13 +17,15 @@ ag <- aggregate(D_biep.White_Good_all~countrycit, gtd, function(x) c(mean = mean
 
 # Join the stats df and ag df
 constat <- join(ag, df2,
-               type = "inner")
+                type = "inner")
 
 # Calculate the standard error field
 constat$err <- constat$D_biep.White_Good_all[,c("sd")]/sqrt(constat$freq)
 
 # Write the consolidated file with Mean IAT score, country code, SD, SE and sample size (Good file to get Idea of data points)
-write.csv(constat, "Consolidatedstat.2004-2015-white-europe.csv", row.names=FALSE, quote=FALSE)
+#write.csv(constat, "Consolidatedstat.2004-2015-white-europe.csv", row.names=FALSE, quote=FALSE)
+# TAking out countries with n< 100
+constat <- constat[-c(42,27,23,3,29,1,25), ]
 
 IATScore = constat$D_biep.White_Good_all[,c("mean")]
 freq = constat$freq
@@ -37,7 +41,7 @@ text(freq, IATScore,
      cex= 0.7, pos=2)
 
 # to get the error intervals, Take out the comment if interested in that
-arrows(freq, IATScore-serr, freq, IATScore+serr, length=0.05, angle=90, code=3)
+#arrows(freq, IATScore-serr, freq, IATScore+serr, length=0.05, angle=90, code=3)
 
 # Use to explore the plot
 library(zoom)
